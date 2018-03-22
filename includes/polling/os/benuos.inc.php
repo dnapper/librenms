5019aa2699891da685a0fda4e7ac504ba67f7f65
dnapper <darren.napper@gmail.com>
2018-03-22 10:35:00 -0600

    updated graph polling with new radius oids
    second pass

--- includes/polling/os/benuos.inc.php
+++ includes/polling/os/benuos.inc.php
@@ -7,3 +7,34 @@ $features = $matches['2'];
 $hardware = $matches['3'];
 
 $serial = snmp_get($device, 'benuChassisId.0', '-Ovqs', 'BENU-CHASSIS-MIB');
+
+$oids = array(
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.6.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.7.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.8.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.9.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.10.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.11.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.12.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.13.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.14.1.1',
+    '.1.3.6.1.4.1.39406.2.1.4.3.2.1.15.1.1',
+);
+
+$radiusacct_data = snmp_get_multi_oid($device, $oids);
+list ($requestrcvd, $requestsent, $startrequestrcvd, $stoprequestrcvd, $interimupdatercvd, $startrequestsent, $stoprequestsent, $interimupdatesent, $responsercvd, $responsesent) = array_values($radiusacct_data);
+
+$rrd_def = RrdDefinition::make()
+    ->addDataset('RequestRcvd', 'GAUGE', 0, 10000000);
+    ->addDataset('RequestSent', 'GAUGE', 0, 10000000);
+
+$fields = array(
+    'RequestRcvd'  => $requestrcvd,
+    'RequestSent'  => $requestsent,
+);
+
+$tags = compact('rrd_def');
+data_update($device, 'benuos_RadiusProxy', $tags, $fields);
+
+
+$graphs['benuos_RadiusProxy'] = true;
